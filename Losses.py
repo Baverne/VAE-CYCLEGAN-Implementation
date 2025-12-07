5d9af0501778d39b9fb47019ac30ce186e3ecda6
Baptiste TAVERNE <baptiste.taverne@polytechnique.edu>
2025-12-07 22:17:55 +0100

    Networks and losses

---
+++ Losses.py
@@ -0,0 +1,321 @@
+"""
+Loss functions for VAE-CycleGAN implementation
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class TranslationLoss(nn.Module):
+    """
+    Translation Loss (L1 reconstruction)
+    L_trans(x,y) = ||G(x) - y||_1
+    """
+    def __init__(self):
+        super(TranslationLoss, self).__init__()
+        self.l1_loss = nn.L1Loss()
+    
+    def forward(self, generated, target):
+        return self.l1_loss(generated, target)
+
+
+class CycleConsistencyLoss(nn.Module):
+    """
+    Cycle Consistency Loss
+    L_cycle = ||F(G(x)) - x||_1 + ||G(F(y)) - y||_1
+    """
+    def __init__(self):
+        super(CycleConsistencyLoss, self).__init__()
+        self.l1_loss = nn.L1Loss()
+    
+    def forward(self, x, y, FGx, GFy):
+        loss_x = self.l1_loss(FGx, x)  # ||F(G(x)) - x||_1
+        loss_y = self.l1_loss(GFy, y)  # ||G(F(y)) - y||_1
+        return loss_x + loss_y
+
+
+class IdentityLoss(nn.Module):
+    """
+    Identity Loss
+    L_id = ||G(x) - x||_1 + ||F(y) - y||_1
+    """
+    def __init__(self):
+        super(IdentityLoss, self).__init__()
+        self.l1_loss = nn.L1Loss()
+    
+    def forward(self, x, y, Gx, Fy):
+        loss_G = self.l1_loss(Gx, x)  # ||G(x) - x||_1
+        loss_F = self.l1_loss(Fy, y)  # ||F(y) - y||_1
+        return loss_G + loss_F
+
+
+class GANLossGenerator(nn.Module):
+    """
+    L_GAN^{X->Y} = D_Y(y)^2 + (1 - D_Y(G(x)))^2
+    L_GAN^{Y->X} = D_X(x)^2 + (1 - D_X(F(y)))^2
+    """
+    def __init__(self):
+        super(GANLossGenerator, self).__init__()
+        self.mse_loss = nn.MSELoss()
+    
+    def forward(self, D_real, D_fake):
+        # D(y)^2 + (1 - D(G(x)))^2
+        real_loss = self.mse_loss(D_real, torch.zeros_like(D_real))
+        fake_loss = self.mse_loss(D_fake, torch.ones_like(D_fake))
+        return real_loss + fake_loss
+
+
+class GANLossDiscriminator(nn.Module):
+    """
+    Adversarial Loss for Discriminators (LSGAN)
+    
+    L_GAN^{D_X} = (1 - D_X(x))^2 + D_X(F(y))^2
+    L_GAN^{D_Y} = (1 - D_Y(y))^2 + D_Y(G(x))^2
+    """
+    def __init__(self):
+        super(GANLossDiscriminator, self).__init__()
+        self.mse_loss = nn.MSELoss()
+    
+    def forward(self, D_real, D_fake):
+        # (1 - D(x))^2 + D(G(x))^2
+        real_loss = self.mse_loss(D_real, torch.ones_like(D_real))
+        fake_loss = self.mse_loss(D_fake, torch.zeros_like(D_fake))
+        return real_loss + fake_loss
+
+
+class KLDivergenceLoss(nn.Module):
+    """
+    KL Divergence Loss for VAE
+    
+    L_KL = D_KL(q(z|x) || N(0,I))
+         = -0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    """
+    def __init__(self):
+        super(KLDivergenceLoss, self).__init__()
+    
+    def forward(self, mu, logvar):
+        # -0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        # Normalize by batch size and dimensions
+        return kl_loss / (mu.size(0) * mu.numel() / mu.size(0))
+
+
+class AELoss(nn.Module):
+    """
+    Loss for AE (paired)
+    L = L_trans
+    """
+    def __init__(self):
+        super(AELoss, self).__init__()
+        self.translation_loss = TranslationLoss()
+    
+    def forward(self, model_output, x, y):
+        Gx = model_output
+        
+        loss_trans = self.translation_loss(Gx, y)
+        
+        total_loss = loss_trans
+        
+        losses_dict = {
+            'loss_total': total_loss.item(),
+            'loss_trans': loss_trans.item()
+        }
+        
+        return total_loss, losses_dict
+
+
+class CycleAELossPaired(nn.Module):
+    """
+    Loss for Cycle AE (paired)
+    L = L_trans + λ_cycle * L_cycle
+    """
+    def __init__(self, lambda_cycle=10.0):
+        super(CycleAELossPaired, self).__init__()
+        self.translation_loss = TranslationLoss()
+        self.cycle_loss = CycleConsistencyLoss()
+        self.lambda_cycle = lambda_cycle
+    
+    def forward(self, model_output, x, y):
+        Gx, FGx, Fy, GFy = model_output
+        
+        loss_trans = self.translation_loss(Gx, y)
+        loss_cycle = self.cycle_loss(x, y, FGx, GFy)
+        
+        total_loss = loss_trans + self.lambda_cycle * loss_cycle
+        
+        losses_dict = {
+            'loss_total': total_loss.item(),
+            'loss_trans': loss_trans.item(),
+            'loss_cycle': loss_cycle.item()
+        }
+        
+        return total_loss, losses_dict
+
+
+class AEGANLoss(nn.Module):
+    """
+    Loss for AE-GAN (paired)
+    L = L_trans + λ_GAN * L_GAN + λ_identity * L_id
+    """
+    def __init__(self, lambda_gan=1.0, lambda_identity=5.0):
+        super(AEGANLoss, self).__init__()
+        self.translation_loss = TranslationLoss()
+        self.gan_loss_gen = GANLossGenerator()
+        self.identity_loss = IdentityLoss()
+        self.lambda_gan = lambda_gan
+        self.lambda_identity = lambda_identity
+    
+    def forward(self, model_output, x, y):
+        Gx, DGx, Dx = model_output
+        
+        loss_trans = self.translation_loss(Gx, y)
+        loss_gan = self.gan_loss_gen(Dx, DGx)
+        # For identity loss, we need G(x) and F(y), but in single generator case:
+        loss_id = self.identity_loss(x, y, Gx, y)  # Simplified - may need adjustment
+        
+        total_loss = loss_trans + self.lambda_gan * loss_gan + self.lambda_identity * loss_id
+        
+        losses_dict = {
+            'loss_total': total_loss.item(),
+            'loss_trans': loss_trans.item(),
+            'loss_gan': loss_gan.item(),
+            'loss_identity': loss_id.item()
+        }
+        
+        return total_loss, losses_dict
+
+
+class VAELoss(nn.Module):
+    """
+    Loss for VAE (paired)
+    L = L_trans + λ_kl * L_KL
+    """
+    def __init__(self, lambda_kl=1e-5):
+        super(VAELoss, self).__init__()
+        self.translation_loss = TranslationLoss()
+        self.kl_loss = KLDivergenceLoss()
+        self.lambda_kl = lambda_kl
+    
+    def forward(self, model_output, x, y):
+        Gx, mu, logvar = model_output
+        
+        loss_trans = self.translation_loss(Gx, y)
+        loss_kl = self.kl_loss(mu, logvar)
+        
+        total_loss = loss_trans + self.lambda_kl * loss_kl
+        
+        losses_dict = {
+            'loss_total': total_loss.item(),
+            'loss_trans': loss_trans.item(),
+            'loss_kl': loss_kl.item()
+        }
+        
+        return total_loss, losses_dict
+
+
+class CycleVAELossPaired(nn.Module):
+    """
+    Loss for Cycle-VAE (paired)
+    L = L_trans + λ_cycle * L_cycle + λ_kl * L_KL
+    """
+    def __init__(self, lambda_cycle=10.0, lambda_kl=1e-5):
+        super(CycleVAELossPaired, self).__init__()
+        self.translation_loss = TranslationLoss()
+        self.cycle_loss = CycleConsistencyLoss()
+        self.kl_loss = KLDivergenceLoss()
+        self.lambda_cycle = lambda_cycle
+        self.lambda_kl = lambda_kl
+    
+    def forward(self, model_output, x, y):
+        Gx, FGx, Fy, GFy, mu_x, logvar_x, mu_FGx, logvar_FGx, mu_y, logvar_y, mu_GFy, logvar_GFy = model_output
+        
+        loss_trans = self.translation_loss(Gx, y)
+        loss_cycle = self.cycle_loss(x, y, FGx, GFy)
+        
+        # KL losses for both domains
+        loss_kl_x = self.kl_loss(mu_x, logvar_x)
+        loss_kl_y = self.kl_loss(mu_y, logvar_y)
+        loss_kl = loss_kl_x + loss_kl_y
+        
+        total_loss = loss_trans + self.lambda_cycle * loss_cycle + self.lambda_kl * loss_kl
+        
+        losses_dict = {
+            'loss_total': total_loss.item(),
+            'loss_trans': loss_trans.item(),
+            'loss_cycle': loss_cycle.item(),
+            'loss_kl': loss_kl.item(),
+            'loss_kl_x': loss_kl_x.item(),
+            'loss_kl_y': loss_kl_y.item()
+        }
+        
+        return total_loss, losses_dict
+
+
+class VAEGANLoss(nn.Module):
+    """
+    Loss for VAE-GAN (paired)
+    L = L_trans + λ_GAN * L_GAN + λ_identity * L_id + λ_kl * L_KL
+    """
+    def __init__(self, lambda_gan=1.0, lambda_identity=5.0, lambda_kl=1e-5):
+        super(VAEGANLoss, self).__init__()
+        self.translation_loss = TranslationLoss()
+        self.gan_loss_gen = GANLossGenerator()
+        self.identity_loss = IdentityLoss()
+        self.kl_loss = KLDivergenceLoss()
+        self.lambda_gan = lambda_gan
+        self.lambda_identity = lambda_identity
+        self.lambda_kl = lambda_kl
+    
+    def forward(self, model_output, x, y):
+        Gx, mu, logvar, DGx, Dx = model_output
+        
+        loss_trans = self.translation_loss(Gx, y)
+        loss_gan = self.gan_loss_gen(Dx, DGx)
+        loss_id = self.identity_loss(x, y, Gx, y)  # Simplified
+        loss_kl = self.kl_loss(mu, logvar)
+        
+        total_loss = (loss_trans + self.lambda_gan * loss_gan + 
+                     self.lambda_identity * loss_id + self.lambda_kl * loss_kl)
+        
+        losses_dict = {
+            'loss_total': total_loss.item(),
+            'loss_trans': loss_trans.item(),
+            'loss_gan': loss_gan.item(),
+            'loss_identity': loss_id.item(),
+            'loss_kl': loss_kl.item()
+        }
+        
+        return total_loss, losses_dict
+
+
+# UNPAIRED MODELS
+
+class CycleAELossUnpaired(nn.Module):
+    """
+    Loss for Cycle AE (unpaired)
+    L = λ_cycle * L_cycle
+    """
+
+
+class AECycleGANLoss(nn.Module):
+    """
+    Loss for AE-CycleGAN (unpaired)
+    L = λ_GAN * L_GAN + λ_identity * L_id + λ_cycle * L_cycle
+    """
+
+
+class CycleVAELossUnpaired(nn.Module):
+    """
+    Loss for Cycle-VAE (unpaired)
+    L = λ_cycle * L_cycle + λ_kl * L_KL
+    """
+
+
+
+class VAECycleGANLoss(nn.Module):
+    """
+    Loss for VAE-CycleGAN (unpaired)
+    L = λ_GAN * L_GAN + λ_identity * L_id + λ_cycle * L_cycle + λ_kl * L_KL
+    """
+
---
+++ Networks.py
@@ -0,0 +1,327 @@
+"""
+All necessary networks for VAE-CygleGAN implementation
+"""
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+import torch.nn as nn
+
+class CaSb (nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=3, activation="ReLU"):
+        super(CaSb, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, padding_mode='reflect')
+        self.norm = nn.InstanceNorm2d(out_channels)
+        if activation == "ReLU":
+            self.activation = nn.ReLU(inplace=True)
+        elif activation == "LeakyReLU":
+            self.activation = nn.LeakyReLU(0.2, inplace=True)
+        elif activation == "Tanh":
+            self.activation = nn.Tanh()
+        else :
+            raise NotImplementedError("Activation not implemented")
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        print(x.shape)
+        return x
+
+class D (nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(D, self).__init__()
+        self.PixelUnshuffle = nn.PixelUnshuffle(downscale_factor=2)
+        self.conv = nn.Conv2d(in_channels*4, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.norm = nn.InstanceNorm2d(out_channels)
+        self.activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.PixelUnshuffle(x)
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        return x
+
+class R (nn.Module):
+    def __init__(self, out_channels):
+        super(R, self).__init__()
+        self.conv1 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.norm1 = nn.InstanceNorm2d(out_channels)
+        self.activation1 = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.norm2 = nn.InstanceNorm2d(out_channels)
+        self.activation2 = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+        x = self.conv1(x)
+        x = self.norm1(x)
+        x = self.activation1(x)
+        x = self.conv2(x)
+        x = self.norm2(x)
+        x = self.activation2(x)
+        x += residual
+        return x
+
+class U (nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(U, self).__init__()
+        self.PixelShuffle = nn.PixelShuffle(upscale_factor=2)
+        self.conv = nn.Conv2d(in_channels//4, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+        self.norm = nn.InstanceNorm2d(out_channels)
+        self.activation = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.PixelShuffle(x)
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.activation(x)
+        return x
+    
+class S (nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(S, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+    
+class L (nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(L, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, padding_mode='reflect')
+
+    def forward(self, x):
+        x = self.conv(x)
+        return x
+
+
+    
+
+class Encoder (nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        layers = []
+        layers.append(CaSb(3, 64, kernel_size=7, stride=1))
+        layers.append(D(64, 128))
+        layers.append(D(128, 256))
+        layers.append(D(256, 512))
+        layers.append(D(512, 1024))
+        layers.append(R(1024))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+    
+class VariationalEncoderBlock (nn.Module):
+    def __init__(self, in_channels, latent_dim):
+        super(VariationalEncoderBlock, self).__init__()
+        self.muConv = L(in_channels, latent_dim)
+        self.logvarConv = nn.Sequential(S(in_channels, latent_dim), S(latent_dim, latent_dim))
+
+    def forward(self, x):
+        mu = self.muConv(x)
+        logvar = self.logvarConv(x)
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        z = mu + eps * std
+        return z, mu, logvar
+    
+class VariationalDecoderBlock (nn.Module):
+    def __init__(self, latent_dim, out_channels):
+        super(VariationalDecoderBlock, self).__init__()
+        self.conv = S(latent_dim, out_channels)
+
+    def forward(self, z):
+        x_recon = self.conv(z)
+        return x_recon
+    
+class Decoder (nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        layers = []
+        layers.append(R(1024))
+        layers.append(U(1024, 512))
+        layers.append(U(512, 256))
+        layers.append(U(256, 128))
+        layers.append(U(128, 64))
+        layers.append(CaSb(64, 3, kernel_size=7, stride=1, activation="Tanh"))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+    
+class Autoencoder (nn.Module):
+    def __init__(self):
+        super(Autoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        z = self.encoder(x)
+        Gx = self.decoder(z)
+        return Gx
+
+class VariationalAutoencoder (nn.Module):
+    def __init__(self, latent_dim):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = Encoder()
+        self.variational_encoder_block = VariationalEncoderBlock(in_channels=1024, latent_dim=latent_dim)
+        self.variational_decoder_block = VariationalDecoderBlock(latent_dim=latent_dim, out_channels=1024)
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        z, mu, logvar = self.variational_encoder_block(encoded)
+        decoded_latent = self.variational_decoder_block(z)
+        Gx = self.decoder(decoded_latent)
+        return Gx, mu, logvar
+    
+class Discriminator (nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        layers = []
+        layers.append(CaSb(3, 64, kernel_size=4, stride=2, padding=1, activation="LeakyReLU"))
+        layers.append(CaSb(64, 128, kernel_size=4, stride=2, padding=1, activation="LeakyReLU"))
+        layers.append(CaSb(128, 256, kernel_size=4, stride=2, padding=1, activation="LeakyReLU"))
+        layers.append(CaSb(256, 512, kernel_size=4, stride=2, padding=1, activation="LeakyReLU"))
+        layers.append(nn.Conv2d(512, 1, kernel_size=16, stride=1, padding=0))
+        
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        #shape (B, 1, 1, 1) to (B,1)
+        return self.model(x).view(-1, 1).squeeze(1)
+    
+class AEGAN (nn.Module):
+    def __init__(self, latent_dim):
+        super(AEGAN, self).__init__()
+        self.G = Autoencoder()
+        self.D = Discriminator()
+
+
+    def forward(self, x):
+        Gx= self.G(x)
+        DGx = self.D(Gx)
+        Dx = self.D(x)
+        return Gx, DGx, Dx
+
+
+class VAEGAN (nn.Module):
+    def __init__(self, latent_dim):
+        super(VAEGAN, self).__init__()
+        self.G = VariationalAutoencoder(latent_dim)
+        self.D = Discriminator()
+        self.latent_dim = latent_dim
+
+    def forward(self, x):
+        Gx, mu, logvar = self.G(x)
+        DGx = self.D(Gx)
+        Dx = self.D(x)
+        return Gx, mu, logvar, DGx, Dx
+
+class CycleAE (nn.Module):
+    def __init__(self):
+        super(CycleAE, self).__init__()
+        self.F = Autoencoder()
+        self.G = Autoencoder()
+    def forward(self, x, y):
+        Gx = self.G(x)
+        FGx = self.F(Gx)
+        Fy = self.F(y)
+        GFy = self.G(Fy)
+        return Gx, FGx, Fy, GFy
+    
+class CycleVAE (nn.Module):
+    def __init__(self, latent_dim):
+        super(CycleVAE, self).__init__()
+        self.F = VariationalAutoencoder(latent_dim)
+        self.G = VariationalAutoencoder(latent_dim)
+    def forward(self, x, y):
+        Gx, mu_x, logvar_x = self.G(x)
+        FGx, mu_FGx, logvar_FGx = self.F(Gx)
+        Fy, mu_y, logvar_y = self.F(y)
+        GFy, mu_GFy, logvar_GFy = self.G(Fy)
+        return Gx, FGx, Fy, GFy, mu_x, logvar_x, mu_FGx, logvar_FGx, mu_y, logvar_y, mu_GFy, logvar_GFy
+
+class CycleAEGAN (nn.Module):
+    def __init__(self):
+        super(CycleAE, self).__init__()
+        self.F = Autoencoder()
+        self.G = Autoencoder()
+        self.DX = Discriminator()
+        self.DY = Discriminator()
+    def forward(self, x, y):
+        Gx = self.G(x)
+        FGx = self.F(Gx)
+        Fy = self.F(y)
+        GFy = self.G(Fy)
+        DYGx = self.DY(Gx)
+        DXFy = self.DX(Fy)
+        DXx = self.DX(x)
+        DYy = self.DY(y)
+        return Gx, FGx, Fy, GFy, DYGx, DXFy
+    
+    
+class CycleVAEGAN (nn.Module):
+    def __init__(self, latent_dim):
+        super(CycleVAEGAN, self).__init__()
+        self.F = VariationalAutoencoder(latent_dim)
+        self.G = VariationalAutoencoder(latent_dim)
+        self.DX = Discriminator()
+        self.DY = Discriminator()
+    def forward(self, x, y):
+        Gx, mu_x, logvar_x = self.G(x)
+        FGx, mu_FGx, logvar_FGx = self.F(Gx)
+        Fy, mu_y, logvar_y = self.F(y)
+        GFy, mu_GFy, logvar_GFy = self.G(Fy)
+        DYGx = self.DY(Gx)
+        DXFy = self.DX(Fy)
+        DXx = self.DX(x)
+        DYy = self.DY(y)
+        return Gx, FGx, Fy, GFy, mu_x, logvar_x, mu_FGx, logvar_FGx, mu_y, logvar_y, mu_GFy, logvar_GFy, DYGx, DXFy, DXx, DYy
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+    # Test the networks
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    x = torch.randn((10, 3, 256, 256)).to(device)
+    encoder = Encoder().to(device)
+    decoder = Decoder().to(device)
+    z = encoder(x)
+    print("Encoded shape:", z.shape)
+    x_recon = decoder(z)
+    print("Reconstructed shape:", x_recon.shape)
+    vae = VariationalAutoencoder(latent_dim=128).to(device)
+    x_recon, mu, logvar = vae(x)
+    print("VAE Reconstructed shape:", x_recon.shape)
+    print("VAE Mu shape:", mu.shape)
+
+    discriminator = Discriminator().to(device)
+    validity = discriminator(x)
+    print("Discriminator output shape:", validity.shape)
+
+    aegan = AEGAN(latent_dim=128).to(device)
+    x_recon, validity = aegan(x)
+    print("AEGAN Reconstructed shape:", x_recon.shape)
+    print("AEGAN Discriminator output shape:", validity.shape)
+    vaegan = VAEGAN(latent_dim=128).to(device)
+    x_recon, mu, logvar, validity = vaegan(x)
+    print("VAEGAN Reconstructed shape:", x_recon.shape)
+    print("VAEGAN Discriminator output shape:", validity.shape)
+    cycle_ae = CycleAE().to(device)
+    y = torch.randn((10, 3, 256, 256)).to(device)
+    Gx, FGx, Fy, GFy = cycle_ae(x, y)
+    print("CycleAE Gx shape:", Gx.shape)
+    print("CycleAE FGx shape:", FGx.shape)
+    print("CycleAE Fy shape:", Fy.shape)
+    print("CycleAE GFy shape:", GFy.shape)
+
